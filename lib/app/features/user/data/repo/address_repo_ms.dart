836cc004375d153b5cdb8d7980bc8b3492c86fdb
Thiennn262626 <ndt10a5@gmail.com>
2023-10-23 17:43:05 +0700

    model address

--- lib/app/features/user/data/repo/address_repo_ms.dart
+++ lib/app/features/user/data/repo/address_repo_ms.dart
@@ -1,8 +1,7 @@
 import 'package:hlshop/all_file/all_file.dart';
-import 'package:hlshop/app/features/user/data/mulstore/api/address_api_ms.dart';
-import 'package:hlshop/app/features/user/data/mulstore/model/address_model_ms.dart';
+import 'package:hlshop/app/features/user/data/api/address_api_ms.dart';
+import 'package:hlshop/app/features/user/data/model/address_model_ms.dart';
 import 'package:hlshop/app/features/user/domain/entity/address_entity.dart';
-import 'package:hlshop/app/features/user/domain/entity/user_base_entity.dart';
 import 'package:hlshop/app/features/user/domain/repo/address_repo.dart';
 
 class AddressRepoMs implements AddressRepo {
@@ -56,5 +55,4 @@ class AddressRepoMs implements AddressRepo {
     }
     throw Exception('không tìm thấy quận');
   }
-
 }
