db6b60d43e274c20de25d9000a846034eaf26132
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2023-12-12 16:07:33 +0700

    fix rebuild tabpageview in order

--- lib/app/features/user_order/presentation/main/user_order_tab.dart
+++ lib/app/features/user_order/presentation/main/user_order_tab.dart
@@ -4,18 +4,27 @@ import 'package:hlshop/app/features/user_order/domain/repo/user_order_repo.dart'
 import 'package:hlshop/app/features/user_order/presentation/group_item/user_order_list_group.dart';
 import 'package:hlshop/app/features/user_order/presentation/main/cubit/user_order_cubit.dart';
 
-class UserOrderTab extends StatelessWidget {
+class UserOrderTab extends StatefulWidget {
   const UserOrderTab({super.key, required this.orderStatus});
 
   final OrderStatus orderStatus;
 
   @override
+  State<UserOrderTab> createState() => _UserOrderTabState();
+}
+
+class _UserOrderTabState extends State<UserOrderTab>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return PagingList<OrderEntity>(
       pagingController:
-          context.read<UserOrderCubit>().controllerMap[orderStatus],
+          context.read<UserOrderCubit>().controllerMap[widget.orderStatus],
       fetchListData: (offset, limit) => getIt<UserOrderRepo>().getOrderList(
-        orderStatus: orderStatus,
+        orderStatus: widget.orderStatus,
         offset: offset,
         limit: limit,
       ),
