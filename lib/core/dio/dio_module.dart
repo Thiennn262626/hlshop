e5854da31313faffea6f0cfbee9d8be40daaa173
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-10 20:28:35 +0700

    Update baseUrl

--- lib/core/dio/dio_module.dart
+++ lib/core/dio/dio_module.dart
@@ -16,12 +16,8 @@ class DioModule {
 
     log('**** Dio create');
     final options = BaseOptions(
-      //baseUrl: 'https://hl-shop.azurewebsites.net/',//deploy
-      // baseUrl: 'http://192.168.1.15/',//phongTro
-      // baseUrl: 'http://172.16.30.61/', //fit
-      baseUrl: 'http://172.20.10.2/', //iphoneThien
-      // baseUrl: 'http://localhost:80/',
-      connectTimeout: const Duration(seconds: 30),
+      baseUrl: 'http://192.168.1.42:80/',  //Qun's ip Address
+       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(seconds: 30),
       sendTimeout: const Duration(seconds: 30),
     );
