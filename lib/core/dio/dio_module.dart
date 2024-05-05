95bea2758129016bcd2d9e95829b14e4099615bf
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-05-06 03:43:03 +0700

    change baseUrl

--- lib/core/dio/dio_module.dart
+++ lib/core/dio/dio_module.dart
@@ -15,7 +15,8 @@ class DioModule {
 
     log('**** Dio create');
     final options = BaseOptions(
-      baseUrl: 'http://192.168.1.7:3000/', //Qun's ip Address
+      // baseUrl: 'http://192.168.1.7:3000/', //Qun's ip Address
+      baseUrl: 'https://hl-backend-a6uq.onrender.com/', //Vercel BE
       // baseUrl: 'http://localhost:80/', //localhost BE
       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(seconds: 30),
