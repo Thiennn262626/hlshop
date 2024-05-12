bc19ce802bff5fe935aee550caa3cbb497cf42ef
Qunnnn <TranVanQuan20211@gmail.com>
2024-05-12 22:56:14 +0700

    set not to userMaterial 3

--- packages/app_ui/lib/src/theme/app_theme.dart
+++ packages/app_ui/lib/src/theme/app_theme.dart
@@ -14,6 +14,7 @@ class AppTheme {
     final textExtensionTheme = AppTextThemeExtension.form(isDark);
     final appColor = AppColorExtension.form(isDark);
     return ThemeData(
+      useMaterial3: false,
       colorScheme: colorScheme,
       fontFamily: fontFamily,
       cupertinoOverrideTheme: CupertinoThemeData(
